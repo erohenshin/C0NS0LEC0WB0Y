67084b6425212b41d7a3ea7dcbf669456e4651bd
Adam Rothra <adamrothra@gmail.com>
2013-07-04 17:19:41 -0700

    format

--- retro/object.c
+++ retro/object.c
@@ -24,7 +24,7 @@ int xCollision(Object *obj1,Object *obj2) {
 
 int yCollision(Object *obj1,Object *obj2) {
 	return obj1->x + obj1->w > obj2->x &&
-		 		 obj1->x < obj2->x + obj2->w && 
-				 obj1->y + obj1->h > obj2->y &&
-		 		 obj1->y < obj2->y + obj2->h;
+	       obj1->x < obj2->x + obj2->w && 
+	       obj1->y + obj1->h > obj2->y &&
+	       obj1->y < obj2->y + obj2->h;
 }
