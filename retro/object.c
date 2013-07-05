3689d376ac5f132b9951a42551bdbf428747e866
Adam Rothra <adamrothra@gmail.com>
2013-07-04 17:18:57 -0700

    format

--- retro/object.c
+++ retro/object.c
@@ -17,9 +17,9 @@ void drawObject(Object *obj,SDL_Rect *clip) {
 
 int xCollision(Object *obj1,Object *obj2) {
 	return obj1->y + obj1->h > obj2->y &&
-		 		 obj1->y < obj2->y + obj2->h &&
-				 obj1->x + obj1->w > obj2->x &&
-		 		 obj1->x < obj2->x + obj2->w;
+	       obj1->y < obj2->y + obj2->h &&
+	       obj1->x + obj1->w > obj2->x &&
+	       obj1->x < obj2->x + obj2->w;
 }
 
 int yCollision(Object *obj1,Object *obj2) {
