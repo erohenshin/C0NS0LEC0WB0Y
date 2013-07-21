aaac7b88f3fbff9dec793b44d1cd7b1fce85020d
Adam Rothra <adamrothra@gmail.com>
2013-07-21 16:17:37 -0700

    initial commit

---
+++ misc/fizzle.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main() {
+	unsigned char i;
+	for(i=0x01;i<=0x64;i++) {
+		if      (!(i%0x0F)) printf("FIZZBUZZ\n");
+		else if (!(i%0x03)) printf("FIZZ\n");
+		else if (!(i%0x05)) printf("BUZZ\n");
+		else                printf("%i\n",i);
+	} return 0;
+}
---
+++ misc/gltest.c
@@ -0,0 +1,61 @@
+//gcc -o test gltest.c -lSDL -lGL -lGLU
+
+#include "SDL/SDL.h" 
+#include "SDL/SDL_opengl.h" 
+
+char running = 1;
+
+SDL_Surface *screen;
+SDL_Event   event;
+
+void initialize() {
+  SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_WM_SetCaption("GLTEST", NULL);
+  screen = SDL_SetVideoMode(640, 480, 32, SDL_OPENGL);
+
+  glClearColor(0,0,0,0);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0, 800, 600, 0, -1, 1);
+  
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+}
+
+void drawSomething() {
+  glTranslatef(0, 0, 0);
+  glBegin(GL_QUADS);
+    glColor4f(1.0, 1.0, 1.0, 1.0);
+    glVertex3f( 0, 0, 0 ); 
+    glVertex3f( 300, 0, 0 ); 
+    glVertex3f( 300, 150, 0 ); 
+    glVertex3f( 0, 150, 0 ); 
+  glEnd();
+  glLoadIdentity();
+}
+
+void handleEvent() {
+  while(SDL_PollEvent(&event)) {
+    if(event.type==SDL_QUIT) { 
+      quit=1;
+    }
+  }
+}
+
+int main(int argc, char* args[]) {
+  initialize();
+  
+  while(running) {
+    handleEvent();    
+    glClear(GL_COLOR_BUFFER_BIT);
+    drawSomething();
+    SDL_GL_SwapBuffers();
+  }
+  
+  SDL_FreeSurface(screen);
+  screen = NULL;
+  
+  SDL_Quit();
+
+  return 0;
+}
