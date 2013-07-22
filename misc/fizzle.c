2d2abbe7535b14648809c5b1a68177364b51cecf
Adam Rothra <adamrothra@gmail.com>
2013-07-21 18:57:59 -0700

    puts

--- misc/fizzle.c
+++ misc/fizzle.c
@@ -3,9 +3,9 @@
 int main() {
   unsigned char i;
   for(i=0x01;i<=0x64;i++) {
-    if      (!(i%0x0F)) printf("FIZZBUZZ\n");
-    else if (!(i%0x03)) printf("FIZZ\n");
-    else if (!(i%0x05)) printf("BUZZ\n");
+    if      (!(i%0x0F)) puts("FIZZBUZZ");
+    else if (!(i%0x03)) puts("FIZZ");
+    else if (!(i%0x05)) puts("BUZZ");
     else                printf("%i\n",i);
   } return 0;
 }
