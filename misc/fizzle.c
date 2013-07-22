1028da8212dbbc61297a2e448e260abaf54ff450
Adam Rothra <adamrothra@gmail.com>
2013-07-21 19:52:01 -0700

    format

--- misc/fizzle.c
+++ misc/fizzle.c
@@ -3,7 +3,7 @@
 int main() {
   char i;
   for(i=0x01;i<=0x64;i++) {
-		switch() {
+    switch() {
     	case (!(i%0x0F)) puts("FIZZBUZZ");
     	case (!(i%0x03)) puts("FIZZ");
     	case (!(i%0x05)) puts("BUZZ");
