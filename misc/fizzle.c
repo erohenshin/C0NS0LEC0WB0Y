ae392663903b9af9fe8ec65a7a88c5a583e41a24
Adam Rothra <adamrothra@gmail.com>
2013-07-21 19:51:06 -0700

    switch

--- misc/fizzle.c
+++ misc/fizzle.c
@@ -3,9 +3,10 @@
 int main() {
   char i;
   for(i=0x01;i<=0x64;i++) {
-    if      (!(i%0x0F)) puts("FIZZBUZZ");
-    else if (!(i%0x03)) puts("FIZZ");
-    else if (!(i%0x05)) puts("BUZZ");
-    else                printf("%i\n",i);
-  } return 0;
+		switch() {
+    	case (!(i%0x0F)) puts("FIZZBUZZ");
+    	case (!(i%0x03)) puts("FIZZ");
+    	case (!(i%0x05)) puts("BUZZ");
+    	else             printf("%i\n",i);
+    } return 0;
 }
