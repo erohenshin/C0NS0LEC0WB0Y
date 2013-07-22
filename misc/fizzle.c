e056b1869ced631e22e3c81165304dabd9dc843f
Adam Rothra <adamrothra@gmail.com>
2013-07-21 19:53:06 -0700

    this again

--- misc/fizzle.c
+++ misc/fizzle.c
@@ -4,9 +4,9 @@ int main() {
   char i;
   for(i=0x01;i<=0x64;i++) {
     switch() {
-    	case (!(i%0x0F)) puts("FIZZBUZZ");
-    	case (!(i%0x03)) puts("FIZZ");
-    	case (!(i%0x05)) puts("BUZZ");
-    	else             printf("%i\n",i);
+      case (!(i%0x0F)) puts("FIZZBUZZ");
+      case (!(i%0x03)) puts("FIZZ");
+      case (!(i%0x05)) puts("BUZZ");
+      else             printf("%i\n",i);
     } return 0;
 }
