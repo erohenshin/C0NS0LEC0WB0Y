34040622549a761743083841e99e78a273fee13e
Adam Rothra <adamrothra@gmail.com>
2013-07-21 19:58:15 -0700

    oops

--- misc/fizzle.c
+++ misc/fizzle.c
@@ -3,11 +3,9 @@
 int main() {
   char i;
   for(i=0x01;i<=0x64;i++) {
-    switch() {
-      case (!(i%0x0F)) puts("FIZZBUZZ");
-      case (!(i%0x03)) puts("FIZZ");
-      case (!(i%0x05)) puts("BUZZ");
-      else             printf("%i\n",i);
-    } return 0;
-  }
+    if      (!(i%0x0F)) puts("FIZZBUZZ");
+    else if (!(i%0x03)) puts("FIZZ");
+    else if (!(i%0x05)) puts("BUZZ");
+    else                printf("%i\n",i);
+  } return 0;
 }
