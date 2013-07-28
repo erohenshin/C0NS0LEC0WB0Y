269492f7aa08f02199e5085e3d814319d9f28ed1
Adam Rothra <adamrothra@gmail.com>
2013-07-27 18:28:53 -0700

    update

--- misc/sdltest.c
+++ misc/sdltest.c
@@ -8,7 +8,7 @@ int main(int argc,char *argv[]) {
 	
 	SDL_Init(SDL_INIT_VIDEO|SDL_INIT_EVENTTHREAD);
 	screen = SDL_SetVideoMode(640,480,32,SDL_HWSURFACE|SDL_DOUBLEBUF);
-	SDL_WM_SetCaption("H4!1-5474N",NULL);
+	SDL_WM_SetCaption("H4!1+5474N",NULL);
 	
 	while(running) {
 		if(SDL_PollEvent(&event)) if(event.type==SDL_QUIT) running = 0;
